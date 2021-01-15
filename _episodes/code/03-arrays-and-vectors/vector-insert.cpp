ee0befab9d8778ba4ef7158ab22bef4aa0252380
Kilian <kilian.lieret@posteo.de>
2021-01-15 11:20:54 +0100

    Remove all namespaces from code

--- _episodes/code/03-arrays-and-vectors/vector-insert.cpp
+++ _episodes/code/03-arrays-and-vectors/vector-insert.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 int main() {
-  vector<double> n_vector {-1.0, -2.0, -3.0};
+  std::vector<double> n_vector {-1.0, -2.0, -3.0};
   n_vector.insert(n_vector.begin(), 100.0);
 
-  cout << "The first vector element is " << n_vector[0] <<
-  " and the size is " << n_vector.size() << endl;
+  std::cout << "The first vector element is " << n_vector[0] <<
+  " and the size is " << n_vector.size() << std::endl;
 
   n_vector.insert(n_vector.begin()+1, 200.0);
-  cout << "The second vector element is " << n_vector[1] <<
-  " and the size is " << n_vector.size() << endl;
+  std::cout << "The second vector element is " << n_vector[1] <<
+  " and the size is " << n_vector.size() << std::endl;
 
   return 0;
 }
--- _episodes/code/hello.cpp
+++ _episodes/code/hello.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 
-using namespace std;
-
 int main() {
-  cout << "hello, world" << endl;
+  std::cout << "hello, world" << std::endl;
   return 0;
 }
